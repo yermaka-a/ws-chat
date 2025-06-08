05a15007baf9b0d61c4b094db5f29358e7e78cff
yermaka <ermak.080@mail.ru>
2025-06-08 20:49:24 +0800

    add Footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+
+import './Footer.scss'
+
+function Footer() {
+
+   return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>WS Chat © 2025. Все права защищены.</p>
+        <div className="links">
+          <a href="https://github.com/yermaka-a" target="_blank" rel="noreferrer">
+            Репозиторий
+          </a>
+          <a href="mailto:ermak.080@mail.com">
+            Контакты
+          </a>
+        </div>
+        <small>Сделано с ❤️ WS chat</small>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer
