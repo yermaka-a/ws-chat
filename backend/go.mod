1ec08fb1c93cb333633ed54c8037567b85b6a92d
yermaka <ermak.080@mail.ru>
2025-06-08 18:57:28 +0800

    implement simple ws server

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/yermaka-a/golang-chat
 
 go 1.24.1
+
+require github.com/gorilla/websocket v1.5.3
---
+++ backend/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/yermaka-a/ws-chat/backend/pkg/websocket"
+)
+
+func serveWS(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("websocket endpoint reqched")
+
+	conn, err := websocket.Upgrade(w, r)
+
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := webSocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Ermaka's full stack chat project")
+	setupRoutes()
+	http.ListenAndServe(":5000", nil)
+
+}
