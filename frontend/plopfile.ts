fc68c14fb55ea5c508134b828ba70083f0312370
yermaka <ermak.080@mail.ru>
2025-06-08 20:47:18 +0800

    add plop

---
+++ frontend/plopfile.ts
@@ -0,0 +1,33 @@
+import {AddActionConfig, NodePlopAPI} from 'plop'
+
+export default function (plop: NodePlopAPI) {
+  plop.setGenerator('component', {
+    description: 'Create a React component with TypeScript',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name?'
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'plop-templates/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/index.ts',
+        templateFile: 'plop-templates/index.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.scss',
+        templateFile: 'plop-templates/Component.scss.hbs',
+        skipIfExists: true, 
+        when: (answers) => answers.createTypes, 
+      } as AddActionConfig,
+    ],
+  });
+};
